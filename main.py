14e5f05b2da73837774a526e490cb1b5af648cca
Ilya_ksilen <ilyaspbume@gmail.com>
2026-07-19 10:46:58 +0300

    функции к спикам и их логгинг

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(levelname)s - %(message)s")
+
 Masks_bass = {
     "simple":[1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0],
     "offbeat": [0,0,1,0],
@@ -6,11 +9,26 @@ Masks_bass = {
     "live13":[1,0,0,1,0,0,1,0,0,1,0,0,0],
     "live16":[1,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0]
 }
+def get_mask_b (name):
+    if name in Masks_bass:
+        return Masks_bass [name]
+    else:
+        logging.warning(f"Маска: {name} не найдена! Использую по умолчанию simple.")
+        return Masks_bass["simpe"]
+
 Masks_lead = {
-    "random1":[],
-    "random2":[],
-    "random3":[]
+    "rand1":[1,1,1,0,0,0,0,0,1,1,1,1,0,0,1,1],
+    "rand2":[1,1,0,0,1,0,1,0],
+    "rand3":[0,0,1,0,0,1,0,1],
+    "rand4":[1,1,0,0],
+    "rand5":[1,1,1,0,0,0,0,0,0,1,1,1,0,0,0,0]
 }
+def get_mask_l (name):
+    if name in Masks_lead:
+        return Masks_lead [name]
+    else:
+        logging.warning(f"Маска {name} не найдена! Использую по умолчанию rand1.") 
+        return Masks_lead ["rand4"]
 
 Pattern_key = {
     "minor": [0,2,3,5,7,8,10],
@@ -18,6 +36,12 @@ Pattern_key = {
     "dor":[0,2,3,5,7,9,10],
     "frig":[0,1,3,5,7,8,10]
 }
+def get_key (name):
+    if name in Pattern_key:
+        return Pattern_key ["minor"]
+    else:
+        logging.warning(f"Лад: {name} не найден! Использую по умолчанию minor.")
+        return Pattern_key ["minor"]    
 
 def generate_bass (config):
 #code
