2a576af6d2982d9ca2932977a9fb7f493904de49
Ilya_ksilen <ilyaspbume@gmail.com>
2026-07-19 11:17:04 +0300

    основной код и функции генерации баса и лида

--- main.py
+++ main.py
@@ -30,20 +30,63 @@ def get_mask_l (name):
         logging.warning(f"Маска {name} не найдена! Использую по умолчанию rand1.") 
         return Masks_lead ["rand4"]
 
-Pattern_key = {
+Scale_key = {
     "minor": [0,2,3,5,7,8,10],
     "major":[0,2,4,5,7,9,11],
     "dor":[0,2,3,5,7,9,10],
     "frig":[0,1,3,5,7,8,10]
 }
 def get_key (name):
-    if name in Pattern_key:
-        return Pattern_key ["minor"]
+    if name in Scale_key:
+        return Scale_key ["minor"]
     else:
-        logging.warning(f"Лад: {name} не найден! Использую по умолчанию minor.")
-        return Pattern_key ["minor"]    
+        logging.warning(f"Scale: {name} не найден! Использую по умолчанию minor.")
+        return Scale_key ["minor"]    
 
 def generate_bass (config):
-#code
-def generate_lead (config): 
-#code
+    root_note = config["root_note"]
+    scale = Scale_key(config["scale"])
+    mask = get_mask_b(config["mask"])
+    steps = config["steps"]
+    oct_shift = config["oct_shift"]
+
+    count_note = 0
+    result=[]
+    for i in range(steps):
+        note_idx = i%len(root_note)
+        scale_idx = i%len(scale)
+        mask_idx = i%len(mask)
+
+        note = root_note[note_idx] + scale[scale_idx] + oct_shift
+
+        if mask[mask_idx] = 1:
+            result.append(note)
+            count_note +=1
+        else:
+            result-append(None)
+    return result,count_note
+
+def generate_lead (config):
+    root_note = config["root_note"]
+    scale = Scale_key(config["scale"])
+    mask = Masks_leaad(config["mask"])
+    steps = config["steps"]
+    oct_shift = config["oct_shift"]
+
+    result=[]
+    count_note=0
+    for i in range(steps):
+        note_idx = i%len(root_note)
+        scale_idx = i%len(scale)
+        mask_idx = i%len(mask)
+
+        note = root_note[note_idx] + scale[scale_idx] + oct_shift
+        if mask = 1:
+            result.append(note)
+            count_note =+1
+        else:
+            result.append(None)
+    return result
+
+
+#далее прописать конфиги для бас линии и для лид линии
