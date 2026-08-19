709991c057e85ab476ef9e2960f26d7329c42d2a
Ilya_ksilen <ilyaspbume@gmail.com>
2026-08-19 18:31:14 +0300

    испрвил опечатки

--- bot_drum.py
+++ bot_drum.py
@@ -2,7 +2,7 @@ import logging
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.types import InLineKeyboardMarkup, InLineKeyboardButton
+from aiogram.types import InlineKeyboardButton
 from aiogram.types import CallbackQuery
 from dotenv import load_dotenv
 import os
@@ -18,12 +18,12 @@ dp = Dispatcher()
 #обработка команды start
 @dp.message(Command("start"))
 async def start_command(message:types.Message):
-    button = InLineKeyboardButton(text="Сгенерировать паттерн", callback_data="generate")
-    keyboard = InLineKeyboardMarkup(inline_keyboard=[[button]])
+    button = InlineKeyboardButton(text="Сгенерировать паттерн", callback_data="generate")
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button]])
     await message.answer("Привет! Жми кнопку, сгенерируем драмку.", reply_markup=keyboard)
 
 #обработка нажатия кнопки
-@dp.callback_query(lambda c: c.data="generate"
+@dp.callback_query(lambda c: c.data =="generate")
 async def process_generate(callback_query: CallbackQuery):
     await callback_query.answer()
 
